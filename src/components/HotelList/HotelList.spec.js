2c96807ab76d25d5a60af570c84d3812cdd1f226
giacomo9999 <gmarchesi888@gmail.com>
2021-01-21 16:18:47 -0500

    HotelList tests completed.

--- src/components/HotelList/HotelList.spec.js
+++ src/components/HotelList/HotelList.spec.js
@@ -8,6 +8,10 @@ describe("HotelList", () => {
     return shallow(<HotelList {...props} />);
   };
 
+  const setupMount = (props = {}) => {
+    return mount(<HotelList {...props} />);
+  };
+
   it("renders the component", () => {
     const wrapper = setup({
       hotels: dummyData,
@@ -15,5 +19,24 @@ describe("HotelList", () => {
       sortMethod: "recommended",
     });
     expect(wrapper.find(".hotel-list").exists()).toBe(true);
+  });
+
+  it("displays an error panel if no hotel data is present", () => {
+    const wrapper = setup({
+      hotels: [],
+      searchTerm: "Marriott",
+      sortMethod: "recommended",
+    });
+    expect(wrapper.find(".hotel-list").exists()).toBe(false);
+  });
+
+  // sample data is at src/test/dummyData
+  it("successfully displays at least one hotel when valid data is present", () => {
+    const wrapper = setup({
+      hotels: dummyData,
+      searchTerm: "Marriott",
+      sortMethod: "recommended",
+    });
+    expect(wrapper.find(".hotel-card").exists()).toBe(true);
   });
 });
