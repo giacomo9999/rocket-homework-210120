bdfeb6478f46e5cacaada52d62a59e8bab2c6ce5
giacomo9999 <gmarchesi888@gmail.com>
2021-01-21 14:58:56 -0500

    Revert to here if dangerous useState soluntion goes bad.

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "./App";
 
 describe("App", () => {
-  const wrapper = shallow(<App />);
+  const wrapperShallow = shallow(<App />);
+  const wrapperMount = mount(<App />);
 
   it("renders the component", () => {
-    expect(wrapper.find(".app-container").exists()).toBe(true);
+    expect(wrapperShallow.find(".app-container").exists()).toBe(true);
+  });
+
+  it("should render a FilterPanel component", () => {
+    expect(wrapperMount.find(".filters").exists()).toBe(true);
+  });
+
+  it("should render a HotelListDisplay component", () => {
+    expect(wrapper.find(".hotel-list").exists()).toBe(true);
   });
 });
