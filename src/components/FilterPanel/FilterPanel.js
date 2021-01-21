c005619f2d0efd33b289438932063ed79721c46b
giacomo9999 <gmarchesi888@gmail.com>
2021-01-21 17:33:07 -0500

    All done.

--- src/components/FilterPanel/FilterPanel.js
+++ src/components/FilterPanel/FilterPanel.js
@@ -13,29 +13,32 @@ const FilterPanel = (props) => {
 
   return (
     <div className="filters">
-      Hotel Name
-      <input
-        className="input"
-        type="text"
-        name="searchTerm"
-        value={props.searchTerm}
-        onChange={onInputChange}
-        maxLength={20}
-      />
-      Sort Results By
-      <select
-        name="sortMethod"
-        value={props.sortMethod}
-        onChange={onInputChange}
-        className="select"
-      >
-        <option value="recommended">Recommended</option>
-        <option value="low-to-high">Price low-to-high</option>
-        <option value="high-to-low">Price high-to-low</option>
-      </select>
-      <button className="button" name="reset" onClick={() => onReset()}>
-        Reset Search Params
-      </button>
+      <form className="h-form">
+        <label className="h-label">Search Term</label>
+        <input
+          className="h-input"
+          type="text"
+          name="searchTerm"
+          value={props.searchTerm}
+          onChange={onInputChange}
+          maxLength={20}
+        />
+        <label className="h-label">Sort Results By</label>
+        <select
+          className="h-select"
+          name="sortMethod"
+          value={props.sortMethod}
+          onChange={onInputChange}
+          className="select"
+        >
+          <option value="recommended">Recommended</option>
+          <option value="low-to-high">Price low-to-high</option>
+          <option value="high-to-low">Price high-to-low</option>
+        </select>
+        <button className="h-btn" name="reset" onClick={() => onReset()}>
+          Reset Search Params
+        </button>
+      </form>
     </div>
   );
 };
